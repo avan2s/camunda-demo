03ba7ed833631162f2c5471adfcc679b1d56ec27
Andreas van Helden <avh@cedros.com>
2017-08-15 16:26:14 +0200

    Initial commit

---
+++ src/main/java/com/example/camunda/camundademo/CamundaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda.camundademo;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/camunda/camundademo/delegates/DelegateLogTime.java
@@ -0,0 +1,21 @@
+package com.example.camunda.camundademo.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Service("delegateLogTime")
+public class DelegateLogTime implements JavaDelegate {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DelegateLogTime.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        LOGGER.info(LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+    }
+}
